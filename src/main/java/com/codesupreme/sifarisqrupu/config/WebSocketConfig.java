4ba41a6bc47f8a944d4827787bd0a0ccf46f1d69
siyashx <siyashxyusifov@gmail.com>
2025-09-03 19:39:13 +0400

    .

--- src/main/java/com/codesupreme/sifarisqrupu/config/WebSocketConfig.java
+++ src/main/java/com/codesupreme/sifarisqrupu/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package com.codesupreme.sifarisqrupu.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,21 +12,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        var scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(1);
-        scheduler.setThreadNamePrefix("ws-heartbeat-");
-        scheduler.initialize();
-
-        config.enableSimpleBroker("/topic")
-                .setTaskScheduler(scheduler)
-                .setHeartbeatValue(new long[]{15000,15000}); // server↔client heartbeat
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic"); // STOMP Broker için kanal
+        config.setApplicationDestinationPrefixes("/app"); // Mesaj yönlendirme prefix
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*"); // withSockJS() YOXDUR
-
+        registry.addEndpoint("/ws")  // WebSocket bağlantı noktası
+                .setAllowedOrigins("*");
     }
 }
