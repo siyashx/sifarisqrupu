94951bd36debf254edd01135a2f9ae848752ecd4
Siyash Yusifov <93227598+siyashx@users.noreply.github.com>
2026-05-01 19:04:08 +0400

    Update WebSocketConfig.java

--- src/main/java/com/codesupreme/sifarisqrupu/config/WebSocketConfig.java
+++ src/main/java/com/codesupreme/sifarisqrupu/config/WebSocketConfig.java
@@ -17,8 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://mototaksi.az");
-    }
+public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*");
+}
 }
